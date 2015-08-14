271994965fa5966f0f6a4231d20b3979eeae3fff
Terry Chen <terrychen@qnap.com>
2015-08-14 09:37:05 +0800

    add a var TIMEOUT to repeatedly execute apt-mirror

--- setup.sh
+++ setup.sh
@@ -2,18 +2,25 @@
 
 # read mirror.list to link /var/www/package folder
 mkdir /var/www/package
-for i in `egrep -o 'https?://[^ ]+' /etc/apt/mirror.list`; do
-    url=${i/http:\/\//''}
-    target='/var/www/package/' 
-
-    IFS='/' read -a distName <<< "$url"
-    dest=$target${distName[-1]}
-    if [ ! -h $dest ]; then 
-        echo "create $dest"
-        ln -s /var/spool/apt-mirror/mirror/$url $dest
-    fi 
-done
-
 sed -i '12s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/package/' /etc/apache2/sites-enabled/000-default.conf
+service apache2 restart
 
-apt-mirror && apache2ctl -D FOREGROUND
+while true; do
+
+    # parse mirror.list to share under /var/www/package path
+    for i in `egrep -o '(rsync|ftp|https?)://[^ ]+' /etc/apt/mirror.list`; do
+        url=${i/http:\/\//''}
+        target='/var/www/package/' 
+
+        IFS='/' read -a distName <<< "$url"
+        dest=$target${distName[-1]}
+        if [ ! -h $dest ]; then 
+            echo "create $dest"
+            ln -s /var/spool/apt-mirror/mirror/$url $dest
+        fi 
+    done
+
+    printf "\n\n====== wait $TIMEOUT to execute apt-mirror command ======\n\n"
+    sleep $TIMEOUT
+    apt-mirror
+done
